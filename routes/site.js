b1f6f24bf68792388144f22cd35036cad6379665
Noah Gwynn <noah.gwynn@gmail.com>
2021-12-16 17:39:44 -0500

    bug fix

--- routes/site.js
+++ routes/site.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = function (app, dynamicStatic) {
     app.get("*", function (_, res) {
-        dynamicStatic.setPath(path.resolve(__dirname, '../main/build'));
-        res.sendFile(path.resolve(__dirname, "../main/build/index.html"))
+        dynamicStatic.setPath(path.resolve(__dirname, './main/build'));
+        res.sendFile(path.resolve(__dirname, "./main/build/index.html"))
     })
 }
