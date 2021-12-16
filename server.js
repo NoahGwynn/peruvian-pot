04d062a8a510095d6735ce8d963c8cd8e5a30b66
Noah Gwynn <noah.gwynn@gmail.com>
2021-12-16 17:43:30 -0500

    bug fix

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const dynamicStatic = require('express-dynamic-static')();
 const bodyParser = require('body-parser');
 const connectDB = require('./mongoose/config/db')
 // Connect to db
