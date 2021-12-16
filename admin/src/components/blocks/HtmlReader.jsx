f03fd9e612eef530aab71eaf768c9c355fdbf349
Noah Gwynn <noah.gwynn@gmail.com>
2021-12-16 16:01:11 -0500

    main only site

--- admin/src/components/blocks/HtmlReader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {decode} from "html-entities";
-
-function HtmlReader({htmlContent, classNames}) {
-    return (
-        <div className={classNames + " data-from-editor"} dangerouslySetInnerHTML={{__html: decode(htmlContent)}}/>);
-}
-
-export default HtmlReader;
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
-const dynamicStatic = require('express-dynamic-static')();
 const connectDB = require('./mongoose/config/db')
 // Connect to db
 connectDB()
 
 // Setup App
 const app = express();
-app.use(dynamicStatic);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const buildPath = "./main/build";
+app.use(express.static(buildPath));
 
 // app.use(function (req, res, next) {
 //   console.log(req.url)
@@ -21,7 +22,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Routes
 require("./routes/recipeApi")(app)
 require("./routes/ingredientsApi")(app)
-require("./routes/sites")(app, dynamicStatic);
 
 
 // Port & socket connections
