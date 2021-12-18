503264b50835d805d8d4079fce4a614a0b83ce8f
Noah Gwynn <noah.gwynn@gmail.com>
2021-12-17 23:20:09 -0500

    bug fix

--- routes/sites.js
+++ routes/sites.js
@@ -4,12 +4,11 @@ module.exports = function (app, dynamicStatic) {
 
     app.get(["/admin", "/admin/*"], function (_, res) {
         dynamicStatic.setPath(path.resolve(__dirname, "../admin/build/"));
-        res.sendFile(path.resolve(__dirname, "../main/build/index.html"))
+        res.sendFile(path.resolve(__dirname, "../admin/build/index.html"))
     })
 
     app.get("*", function (_, res) {
         dynamicStatic.setPath(path.resolve(__dirname, "../main/build/"));
-        console.log(path.resolve(__dirname, "./main/build/index.html"));
         res.sendFile(path.resolve(__dirname, "../main/build/index.html"))
     })
 
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express');
 const dynamicStatic = require('express-dynamic-static')();
 const bodyParser = require('body-parser');
-const path = require('path');
 const connectDB = require('./mongoose/config/db')
 connectDB()
 
