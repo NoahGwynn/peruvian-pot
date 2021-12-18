8da34b0f9f48af97a2dcdead4a34d7519d757fcd
Noah Gwynn <noah.gwynn@gmail.com>
2021-12-17 20:54:16 -0500

    bug fix

--- routes/sites.js
+++ routes/sites.js
@@ -10,7 +10,7 @@ module.exports = function (app, dynamicStatic) {
     app.get(["/", "/*"], function (_, res) {
         dynamicStatic.setPath(path.resolve(__dirname, "../main/build/"));
         console.log(path.resolve(__dirname, "./main/build/index.html"));
-        res.sendFile(path.resolve("../main/build/index.html"))
+        res.sendFile(path.resolve(__dirname, "../main/build/index.html"))
     })
 
     app.post("/reset-to-main", function (req, res) {
